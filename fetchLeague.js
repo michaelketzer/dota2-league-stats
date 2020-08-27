22cba38f629a91d5d0d62c862f23cbf53be1ed76
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-08-27 22:22:56 +0200

    Added page loading for league ids;

--- fetchLeague.js
+++ fetchLeague.js
@@ -11,17 +11,25 @@ const argv = yargs
             type: 'number',
         }
     })
+    .command('page', 'The page to be queried', {
+        year: {
+            description: 'The page the matches are queried for',
+            alias: 'l',
+            type: 'number',
+        }
+    })
     .help()
     .demandOption(['league'], 'Please define a league id with -l or -league')
     .alias('league', 'l')
+    .alias('page', 'p')
     .alias('help', 'h')
     .argv;
 
-const downloadFile = (async (leagueId) => {
+const downloadFile = (async (leagueId, page = 0) => {
     console.log(chalk.blueBright('Requesting league matches for', leagueId));
-    const res = await fetch(`https://api.stratz.com/api/v1/league/${leagueId}/matches?take=250&include=PLAYER,TEAM`);
+    const res = await fetch(`https://api.stratz.com/api/v1/league/${leagueId}/matches?take=250&include=PLAYER,TEAM&skip` + page * 250);
     if(res.ok) {
-        const fileStream = fs.createWriteStream(__dirname + '/leagues/' + leagueId + '.json' );
+        const fileStream = fs.createWriteStream(__dirname + '/leagues/' + leagueId + (page > 0 ? '_' + page : '') + '.json' );
         fileStream.on('error', function(err) {
             console.log(chalk.red('Error creating write stream', err));
         });
@@ -42,4 +50,4 @@ const downloadFile = (async (leagueId) => {
     }
 });
 
-downloadFile(argv.league);
+downloadFile(argv.league, argv.page);
