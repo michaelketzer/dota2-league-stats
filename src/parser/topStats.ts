1117b104f4e7d5ae25428cc61a7e8b7613573532
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-08-29 20:49:59 +0200

    Added more stats;

--- src/parser/topStats.ts
+++ src/parser/topStats.ts
@@ -10,6 +10,9 @@ let topStats = {
     net: {count: 0, matchId: 0, playerId: 0},
     cs: {count: 0, matchId: 0, playerId: 0},
     cd: {count: 0, matchId: 0, playerId: 0},
+    maxHeal: {count: 0, matchId: 0, playerId: 0},
+    maxTDmg: {count: 0, matchId: 0, playerId: 0},
+    maxHDmg: {count: 0, matchId: 0, playerId: 0},
 }
 
 export function parseTopStats(game: Match): void {
@@ -70,6 +73,27 @@ export function parseTopStats(game: Match): void {
                 playerId: player.steamAccountId,
             }
         }
+        if(player.heroHealing > topStats.maxHeal.count) {
+            topStats.maxHeal = {
+                count: player.heroHealing,
+                matchId: game.id,
+                playerId: player.steamAccountId,
+            }
+        }
+        if(player.towerDamage > topStats.maxTDmg.count) {
+            topStats.maxTDmg = {
+                count: player.towerDamage,
+                matchId: game.id,
+                playerId: player.steamAccountId,
+            }
+        }
+        if(player.heroDamage > topStats.maxHDmg.count) {
+            topStats.maxHDmg = {
+                count: player.heroDamage,
+                matchId: game.id,
+                playerId: player.steamAccountId,
+            }
+        }
     });
 }
 
@@ -77,12 +101,15 @@ export function printTopStats(): void {
     console.log(chalk.blueBright(''));
     console.log(chalk.cyan('Top Player Stats', chalk.magenta('Match id'), chalk.greenBright('Player id')));
     console.log(chalk.blueBright('-------------'));
-    console.log(chalk.blueBright('Top Kills:'), chalk.yellow(topStats.kills.count), chalk.magenta(topStats.kills.matchId), chalk.greenBright(topStats.kills.playerId));
-    console.log(chalk.blueBright('Top Deaths:'), chalk.yellow(topStats.deaths.count), chalk.magenta(topStats.deaths.matchId), chalk.greenBright(topStats.deaths.playerId));
-    console.log(chalk.blueBright('Top Assists:'), chalk.yellow(topStats.assists.count), chalk.magenta(topStats.assists.matchId), chalk.greenBright(topStats.assists.playerId));
+    console.log(chalk.blueBright('Top kills:'), chalk.yellow(topStats.kills.count), chalk.magenta(topStats.kills.matchId), chalk.greenBright(topStats.kills.playerId));
+    console.log(chalk.blueBright('Top deaths:'), chalk.yellow(topStats.deaths.count), chalk.magenta(topStats.deaths.matchId), chalk.greenBright(topStats.deaths.playerId));
+    console.log(chalk.blueBright('Top assists:'), chalk.yellow(topStats.assists.count), chalk.magenta(topStats.assists.matchId), chalk.greenBright(topStats.assists.playerId));
     console.log(chalk.blueBright('Top CS:'), chalk.yellow(topStats.cs.count), chalk.magenta(topStats.cs.matchId), chalk.greenBright(topStats.cs.playerId));
     console.log(chalk.blueBright('Top CD:'), chalk.yellow(topStats.cd.count), chalk.magenta(topStats.cd.matchId), chalk.greenBright(topStats.cd.playerId));
     console.log(chalk.blueBright('Top GPM:'), chalk.yellow(topStats.gpm.count), chalk.magenta(topStats.gpm.matchId), chalk.greenBright(topStats.gpm.playerId));
     console.log(chalk.blueBright('Top XPM:'), chalk.yellow(topStats.xpm.count), chalk.magenta(topStats.xpm.matchId), chalk.greenBright(topStats.xpm.playerId));
-    console.log(chalk.blueBright('Top Net:'), chalk.yellow(topStats.net.count), chalk.magenta(topStats.net.matchId), chalk.greenBright(topStats.net.playerId));
+    console.log(chalk.blueBright('Top net:'), chalk.yellow(topStats.net.count), chalk.magenta(topStats.net.matchId), chalk.greenBright(topStats.net.playerId));
+    console.log(chalk.blueBright('Top heal:'), chalk.yellow(topStats.maxHeal.count), chalk.magenta(topStats.maxHeal.matchId), chalk.greenBright(topStats.maxHeal.playerId));
+    console.log(chalk.blueBright('Top tower dmg:'), chalk.yellow(topStats.maxTDmg.count), chalk.magenta(topStats.maxTDmg.matchId), chalk.greenBright(topStats.maxTDmg.playerId));
+    console.log(chalk.blueBright('Top hero dmg:'), chalk.yellow(topStats.maxHDmg.count), chalk.magenta(topStats.maxHDmg.matchId), chalk.greenBright(topStats.maxHDmg.playerId));
 }
