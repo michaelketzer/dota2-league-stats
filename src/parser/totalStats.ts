a43b5aafe355e10ffccd4022c0e886fb3caad5fb
Michael Ketzer <mk@Michaels-MacBook-Pro.local>
2020-08-29 20:49:59 +0200

    Added more stats;

--- src/parser/totalStats.ts
+++ src/parser/totalStats.ts
@@ -12,6 +12,9 @@ let totalStats = {
     cs: 0,
     cd: 0,
     towers: 0,
+    towerDmg: 0,
+    heroHeal: 0,
+    heroDmg: 0,
 }
 
 export function parseTotalStats(game: Match): void {
@@ -25,6 +28,9 @@ export function parseTotalStats(game: Match): void {
         totalStats.net += player.networth;
         totalStats.cs += player.numLastHits;
         totalStats.cd += player.numDenies;
+        totalStats.heroHeal += player.heroHealing;
+        totalStats.heroDmg += player.heroDamage;
+        totalStats.towerDmg += player.towerDamage;
     });
     totalStats.towers += game.playbackData.towerDeathEvents.length;
 }
@@ -33,14 +39,17 @@ export function printTotalStats(): void {
     console.log(chalk.blueBright(''));
     console.log(chalk.cyan('Total Player Stats'));
     console.log(chalk.blueBright('-------------'));
-    console.log(chalk.blueBright('Total Kills:'), chalk.yellow(totalStats.kills));
-    console.log(chalk.blueBright('Total Deaths:'), chalk.yellow(totalStats.deaths));
-    console.log(chalk.blueBright('Total Assists:'), chalk.yellow(totalStats.assists));
+    console.log(chalk.blueBright('Total kills:'), chalk.yellow(totalStats.kills));
+    console.log(chalk.blueBright('Total deaths:'), chalk.yellow(totalStats.deaths));
+    console.log(chalk.blueBright('Total assists:'), chalk.yellow(totalStats.assists));
     console.log(chalk.blueBright('Total CS:'), chalk.yellow(totalStats.cs));
     console.log(chalk.blueBright('Total CD:'), chalk.yellow(totalStats.cd));
-    console.log(chalk.blueBright('Total Towers:'), chalk.yellow(totalStats.towers));
-    console.log(chalk.blueBright('Total Gold:'), chalk.yellow(totalStats.gold));
-    console.log(chalk.blueBright('Total Gold spent:'), chalk.yellow(totalStats.goldSpent));
+    console.log(chalk.blueBright('Total towers:'), chalk.yellow(totalStats.towers));
+    console.log(chalk.blueBright('Total gold:'), chalk.yellow(totalStats.gold));
+    console.log(chalk.blueBright('Total gold spent:'), chalk.yellow(totalStats.goldSpent));
     console.log(chalk.blueBright('Total xp:'), chalk.yellow(Math.floor(totalStats.xp)));
     console.log(chalk.blueBright('Total net:'), chalk.yellow(totalStats.net));
+    console.log(chalk.blueBright('Total healing:'), chalk.yellow(totalStats.heroHeal));
+    console.log(chalk.blueBright('Total hero dmg:'), chalk.yellow(totalStats.heroDmg));
+    console.log(chalk.blueBright('Total tower dmg:'), chalk.yellow(totalStats.towerDmg));
 }
