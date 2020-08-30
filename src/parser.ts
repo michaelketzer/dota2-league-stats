2c834867c38bd756fceba0111d5c725c0846931b
Michael Ketzer <mk@Michaels-MacBook-Pro.local>
2020-08-30 11:21:54 +0200

    Added region filter;

--- src/parser.ts
+++ src/parser.ts
@@ -8,6 +8,7 @@ import { parseCourierStats, printCourierStats } from './parser/courierStats';
 import { printTopStats, parseTopStats } from './parser/topStats';
 import { parseTotalStats, printTotalStats } from './parser/totalStats';
 import { parseTopHeroes, printTopHeroes } from './parser/topHeroes';
+import { LeagueMatch } from './@types/LeagueMatches';
 
 const argv = yargs
     .command('league', 'The league you want to collect the rosh state for', {
@@ -17,15 +18,23 @@ const argv = yargs
             type: 'number',
         }
     })
+    .command('region', 'Applies a region filter for the games to be looked up', {
+        year: {
+            description: 'Applies a region filter for the games to be looked up',
+            alias: 'l',
+            type: 'number',
+        }
+    })
     .help()
     .demandOption(['league'], 'Please define a league id with -l or -league')
     .alias('league', 'l')
+    .alias('region', 'r')
     .alias('help', 'h')
     .argv;
 
 const data = fs.readFileSync(__dirname + '/../leagues/' + argv.league + '.json');
-const json = JSON.parse(data as unknown as string);
-const games = json.map(({id}) => id);
+const json: LeagueMatch[] = JSON.parse(data as unknown as string);
+const games = json.filter(({regionId}) => !argv.region || +argv.region === regionId ).map(({id}) => id);
 
 console.log(chalk.red('Collecting league stats for ', argv.league));
 console.log(chalk.red('------------------------------------------'));
